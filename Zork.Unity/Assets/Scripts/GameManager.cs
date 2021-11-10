e6bc58aeaffad247dba93b52f461d8027b4dc336
EsavUCF <70274517+EsavUCF@users.noreply.github.com>
2021-11-10 11:48:12 -0500

    Added using Zork implementation to Unity Script and barebones UI

---
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zork;
+public class GameManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Awake()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
